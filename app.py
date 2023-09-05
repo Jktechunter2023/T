99703015a0b3cf810a701cfc052f4a50521b4130
Kafando Rodrique <rdiuskaf@gmail.com>
2023-09-05 16:07:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from src.airplane_poc import my_app
 import streamlit as st
 from src.sms_alert import send_sms
 import os
-os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 
 if __name__ == "__main__":
 
